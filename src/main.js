e51e105d1656d211db6a829137551bb6fc81f44f
Timothy Pendarvis <trpendarvis@gmail.com>
2022-09-01 15:16:41 -0600

    completed fully functional randomizer load and click iteration 0 complete

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ var randomCoverButton = document.querySelector(".random-cover-button")
 // var bookCover = document.querySelector();
 var coverImage = document.querySelector(".cover-image");
 var coverTitle = document.querySelector(".cover-title");
-// var coverTagLine1 = document.querySelector(".tagline-1");
-// var coverTagLine2 = document.querySelector(".tagline-2");
+var coverTagLine1 = document.querySelector(".tagline-1");
+var coverTagLine2 = document.querySelector(".tagline-2");
 // var priceTag = document.querySelector(".price-tag");
 // var overlay = document.querySelector(".overlay");
-var descriptor1 = document.querySelector(".descriptor1");
-var descriptor2 = document.querySelector(".descriptor2");
+// var descriptor1 = document.querySelector(".descriptor1");
+// var descriptor2 = document.querySelector(".descriptor2");
 
 // When the page loads, we should see a cover with a randomly selected image, title, and tagline which includes two random descriptors
 // Every time the user clicks the Show New Random Cover button, a new random cover is created
@@ -24,18 +24,21 @@ var currentCover;
 
 // Add your event listeners here 👇
 
-window.addEventListener("load", showRandomCover);
-window.addEventListener("load", newCoverTitle);
-window.addEventListener("load", new);
+window.addEventListener("load", showRandom);
+randomCoverButton.addEventListener("click", showRandom);
+
 // Create your event handlers and other functions here 👇
 
-function showRandomCover() {
+function showRandom() {
     var newCover = covers[getRandomIndex(covers)];
     var newTitle = titles[getRandomIndex(titles)];
-    var newDescriptor1 = descriptor[];
-    var newDescriptor2 = descriptor
+    var newCoverTagLine1 = descriptors[getRandomIndex(descriptors)];
+    var newCoverTagLine2 = descriptors[getRandomIndex(descriptors)];
+
 coverImage.src = newCover;
 coverTitle.innerText = newTitle;
+coverTagLine1.innerText = newCoverTagLine1;
+coverTagLine2.innerText = newCoverTagLine2;
 }
 // We've provided one function to get you started
 function getRandomIndex(array) {
