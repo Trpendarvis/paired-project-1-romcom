a547c5732409bcd290961e9b33a0954be58f64cb
Timothy Pendarvis <trpendarvis@gmail.com>
2022-09-01 14:37:29 -0600

    this is a testing commit

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ var descriptor2 = document.querySelector(".descriptor2");
 // When the page loads, we should see a cover with a randomly selected image, title, and tagline which includes two random descriptors
 // Every time the user clicks the Show New Random Cover button, a new random cover is created
 // build a randomizer
+
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -24,13 +25,17 @@ var currentCover;
 // Add your event listeners here 👇
 
 window.addEventListener("load", showRandomCover);
-
+window.addEventListener("load", newCoverTitle);
+window.addEventListener("load", new);
 // Create your event handlers and other functions here 👇
 
 function showRandomCover() {
     var newCover = covers[getRandomIndex(covers)];
-
+    var newTitle = titles[getRandomIndex(titles)];
+    var newDescriptor1 = descriptor[];
+    var newDescriptor2 = descriptor
 coverImage.src = newCover;
+coverTitle.innerText = newTitle;
 }
 // We've provided one function to get you started
 function getRandomIndex(array) {
